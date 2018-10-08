a345c40a89577fd372e14c5f051bfecaf2b324be
Ekta Aggarwal <ektaaggarwal24@yahoo.com>
2018-10-08 00:12:56 -0700

    Added the training model for SAP platform

--- IDSGenerator/IDSGenerator.py
+++ IDSGenerator/IDSGenerator.py
@@ -7,7 +7,9 @@ from hmmlearn import hmm
 def get_all_path_files(trainingFolderPath):
     return glob.glob(trainingFolderPath)
 
-def get_features(allTrainingFiles,logicalProperties):
+def get_data(allTrainingFiles,logicalProperties):
+# This function returns the data from all the files mentioned in 'allTrainingFiles'.
+# It further trims the data based on the columns mentioned in 'logicalProperties'.
     features = []
     lengths = []
     for f in allTrainingFiles:
@@ -18,7 +20,10 @@ def get_features(allTrainingFiles,logicalProperties):
     return features,lengths
 
 def train_hmm(allTrainingFiles, logicalProperties, threshold):
-    features,lengths = get_features(allTrainingFiles,logicalProperties)
+# This method creates the Hidden Markov Model (HMM) from the data present in files 'allTrainingFiles' 
+# and uses the properties mentioned in 'logicalProperties'.
+# 'threshold' stands the delta of logLikelihood of two consecutive models and can be used to tune the HMM.
+    features,lengths = get_data(allTrainingFiles,logicalProperties)
     features = np.concatenate(features,axis=0)
     hiddenStates = 2
     prevLikelihood = None
@@ -32,8 +37,8 @@ def train_hmm(allTrainingFiles, logicalProperties, threshold):
             hiddenStates = hiddenStates + 1
             continue
         currentDelta = abs((currentLikelihood - prevLikelihood)*100./prevLikelihood)
-        #print("currentDelta: "+ str(currentDelta))
-        #print("hiddenStates: "+ str(hiddenStates))
+        print("currentDelta: " + str(currentDelta))
+        print("hiddenStates: " + str(hiddenStates))
         if currentDelta < threshold:
             break
         prevLikelihood = currentLikelihood
@@ -41,13 +46,17 @@ def train_hmm(allTrainingFiles, logicalProperties, threshold):
     return model 
     
 def get_mean_std(HMMModel, allTrainingFiles, logicalProperties):
-    features, lengths = get_features( allTrainingFiles, logicalProperties)
+# This method returns the mean and standard deviation of the logLikelihood of the trained HMM
+# given as input 'HMMModel'. Both these values will be used during intrusion detection.
+    features, lengths = get_data( allTrainingFiles, logicalProperties)
     prob = []
     for feature,f in zip(features, allTrainingFiles):
         prob.append(HMMModel.score(feature))
     return np.mean(prob),np.std(prob)
 
-def trainHMMModel(trainingFolderPath, logicalProperties, threshold):
+def train_HMM_model(trainingFolderPath, logicalProperties, threshold):
+# This is the main method which initiates the generation of the trained HMM and returns the trained
+# HMM and its mean logLikelihood.
     allTrainingFiles = get_all_path_files ( trainingFolderPath )
     statsArray = []
     currentHMMModel = train_hmm ( allTrainingFiles, logicalProperties, threshold )
@@ -55,28 +64,31 @@ def trainHMMModel(trainingFolderPath, logicalProperties, threshold):
     return currentHMMModel, statsArray
 
 
+def main(inputPlatform):
+    platform = inputPlatform.upper()
+    idsGeneratorFilePath = os.getcwd()
+    basedir = os.path.dirname(os.path.abspath(idsGeneratorFilePath))
 
-platform = os.path.basename(sys.argv[1]).upper()
-idsGeneratorFilePath = os.getcwd()
-basedir = os.path.dirname(os.path.abspath(idsGeneratorFilePath))
+    parametersFile = open(basedir + "/parameters.txt", "r" )
+    parameters = json.load(parametersFile)
 
-parametersFile = open(basedir + "/parameters.txt", "r" )
-parameters = json.load(parametersFile)
+    logicalProperties = parameters[platform]["logical_properties"]
+    trainingPath = basedir + parameters[platform]["training_IDS_path"]
+    threshold = parameters[platform]["training_threshold"]
 
-logicalProperties = parameters[platform]["logical_properties"]
-trainingPath = basedir + parameters[platform]["training_IDS_path"]
-threshold = parameters[platform]["training_threshold"]
+    print("Generating intrusion detection model..")
+    trainedHMM,statsArray = train_HMM_model(trainingPath, logicalProperties, threshold)
 
-print("Generating intrusion detection model..")
-trainedHMM,statsArray = trainHMMModel(trainingPath, logicalProperties, threshold)
+    trainedHMMFilePath = basedir + "/CORGIDSModel/" + platform + "/trainedModel" + platform + ".pickle"
+    trainedHMMStatsFilePath = basedir + "/CORGIDSModel/" + platform + "/trainedModelStats" + platform + ".pickle"
 
-trainedHMMFilePath = basedir + "/CORGIDSModel/" + platform + "/trainedModel" + platform + ".pickle"
-trainedHMMStatsFilePath = basedir + "/CORGIDSModel/" + platform + "/trainedModelStats" + platform + ".pickle"
+    with open(trainedHMMFilePath, 'wb') as handle:
+        pickle.dump(trainedHMM, handle, protocol=pickle.HIGHEST_PROTOCOL)
+    with open(trainedHMMStatsFilePath, 'wb') as handle2:
+        pickle.dump(statsArray, handle2, protocol=pickle.HIGHEST_PROTOCOL)
 
-with open(trainedHMMFilePath, 'wb') as handle:
-    pickle.dump(trainedHMM, handle, protocol=pickle.HIGHEST_PROTOCOL)
-with open(trainedHMMStatsFilePath, 'wb') as handle2:
-    pickle.dump(statsArray, handle2, protocol=pickle.HIGHEST_PROTOCOL)
+    print("Intrusion detection model generated!")
 
-print("Intrusion detection model generated!")
+if __name__ == '__main__':
+    main(sys.argv[1])
 
--- README.md
+++ README.md
@@ -1,3 +1,10 @@
 # CORGIDS
 
 -----The following resource and CORGIDS source code was developped by Ekta Aggarwal----
+
+## Steps to use CORGIDS:
+### 1. Install hmm-learn library.
+### 2. Clone the CORGIDS repository (git clone https://github.com/ektaaggarwal24/CORGIDS.git). In the steps below "root-dir" refers to the source directory of the CORGIDS local copy.
+### 3. Run python root-dir/IDSGenerator/IDSGenerator.py <PLATFORM>
+PLATFORM here refers to value which can be either "UAV" or "SAP". Based on this argument, the IDS will be generated either for a UAV or a SAP.
+ 
